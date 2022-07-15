f3907fca22147947e3c5da9c9b3354097da258ce
Erkan <erkan.salihoglu@hotmail.com>
2022-07-15 21:18:38 +0300

    44. Ders yapıldı. Profile sayfası düzenlemeleri başlanıldı.

---
+++ TraversalCoreProject/ViewComponents/MemberDashboard/_ProfileInformation.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.MemberDashboard
+{
+    public class _ProfileInformation : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public _ProfileInformation(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.memberName = values.Name + " " + values.Surname;
+            ViewBag.phone = values.PhoneNumber;
+            ViewBag.email = values.Email;
+            
+            
+            return View();
+        }
+    }
+}
