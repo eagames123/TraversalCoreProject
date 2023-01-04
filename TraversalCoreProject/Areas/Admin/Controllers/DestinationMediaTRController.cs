969ce0f35e04255593809652201e51fa9eb57d6c
esisu <erkan.salihoglu@istinye.edu.tr>
2023-01-04 17:05:33 +0300

    75. ders yarıda kaldı
    devam edıyor meditr konusu devam ediyor

---
+++ TraversalCoreProject/Areas/Admin/Controllers/DestinationMediaTRController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TraversalCoreProject.CQRS.Queries.GuideQueries;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    public class DestinationMediaTRController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public DestinationMediaTRController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _mediator.Send(new GetAllGuideQuery());
+            return View(values);
+        }
+    }
+}
