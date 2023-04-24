b3447ecb1fd8c9414f93c68c8656818b80f11da7
ISTINYE\erkan.salihoglu <erkan.salihoglu@istinye.edu.tr>
2023-04-24 13:25:20 +0300

    89. ders yapılacak
    84-88 Signlr dersleri by geçildi. Postgre sql kullanıldığından

---
+++ TraversalCoreProje/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            List<AppRole> values = _roleManager.Roles.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<AppRole> values = _roleManager.Roles.ToList();
+            return View(values);
+        }
+
+    }
+}
